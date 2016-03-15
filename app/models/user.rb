2b2fe7a68bc8a25ec42240f46e977de5cb0ccafa
Brian W <bwuollet@is-mbp-bwuollet5.local>
2016-03-15 11:44:41 -0400

    Add names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 	has_many :pins
+
+	validates :name, presence: true
+	validates :email, uniqueness: true
 end
